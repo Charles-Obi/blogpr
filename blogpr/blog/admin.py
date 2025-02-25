80790dbd7f38ed3dd94c947a76ee09cc516c85b0
Charles-Obi <charleswilliamsobiero@gmail.com>
2025-02-25 11:04:55 +0300

    Add Post model and admin configuration

--- blogpr/blog/admin.py
+++ blogpr/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
+class PostModel(admin.ModelAdmin):
+    list_display = ['title', 'author', 'date_created']
+    search_fields = ['title', 'author']
+    
 # Register your models here.
+admin.site.register(Post, PostModel)
